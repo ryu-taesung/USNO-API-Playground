76d08f6179c95beae6c7fb9ffee70515961096e6
Ryu, Tae Sung <ryu.taesung@gmail.com>
2024-01-15 21:40:34 -0500

    update file path

--- tkinter_astro_data.py
+++ tkinter_astro_data.py
@@ -8,6 +8,7 @@ import xml.etree.ElementTree as ET
 import threading
 import time
 import datetime as dt
+from os import path
 
 
 class LabelInput(tk.Frame):
@@ -53,7 +54,7 @@ class Application(tk.Tk):
         }
         self.zip_code = "00000"
         try:
-            with open('latlong.txt', 'r') as file:
+            with open(path.join(path.dirname(__file__),'latlong.txt'), 'r') as file:
                 json_file = file.read().strip()
                 json_data = json.loads(json_file)
                 self._vars['lat_long'].set(json_data['lat_long'])
@@ -148,7 +149,7 @@ class Application(tk.Tk):
                 self.result.delete('1.0', tk.END)
                 self.result.insert(
                     tk.END, f'Found {self.lat_long}\nPress <Request Data> to get astronomical information for this location.')
-                with open('latlong.txt', 'w', encoding='utf-8') as f:
+                with open(path.join(path.dirname(__file__),'latlong.txt'), 'w', encoding='utf-8') as f:
                     json_out = {'zip_code': self.zip_code,
                                 'lat_long': self.lat_long}
                     f.write(json.dumps(json_out))
